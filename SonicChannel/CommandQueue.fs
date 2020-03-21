724b59b52f1c73ac4a33bc8392d2c35da319473c
ZeekoZhu <vaezt@outlook.com>
2020-03-21 10:18:40 +0800

    refactor: extract CommandQueue

---
+++ SonicChannel/CommandQueue.fs
@@ -0,0 +1,105 @@
+namespace SonicChannel
+open System
+open System.Threading
+open System.Threading.Tasks
+open FSharp.Control.Tasks.V2.ContextInsensitive
+open FSharpx
+open SonicChannel.SonicCommand
+type CSList<'a> = System.Collections.Generic.List<'a>
+
+type SonicCommandCallback =
+    { Command: ISonicCommand; Callback: TaskCompletionSource<ISonicCommand>; }
+
+type CommandQueue(sendMsgFn: string -> Task<unit>) =
+    let mutable disposed = false
+    let mutable waiting: SonicCommandCallback option = None
+    let mutable mailbox: MailboxProcessor<_> option = None
+    let ensureInitialized () =
+        Option.getOrRaise (InvalidOperationException("Transceiver not initialized")) mailbox
+    let waitingLock = new SemaphoreSlim(1, 1)
+    let pendingQueue = CSList<SonicCommandCallback>()
+    let onResponseArrived (line: string) =
+        let handleMessage (cb: SonicCommandCallback) (handleFn: string -> MessageHandleResult) =
+            let handleResult = handleFn line
+            match handleResult with
+            | Bypass -> None
+            | Handled state ->
+                match state with
+                | Pending ->
+                    pendingQueue.Add cb
+                    waiting <- None
+                | Finished ->
+                    waiting <- None
+                Some cb
+
+        let checkWaiting () =
+            waiting
+            |> Option.bind (fun waiting -> handleMessage waiting waiting.Command.HandleWaitingMsg)
+
+        let checkPendingQueue () =
+            let cb =
+                pendingQueue
+                |> Seq.map (fun cb -> handleMessage cb cb.Command.HandlePendingMsg)
+                |> Seq.tryFind (Option.isSome)
+                |> Option.bind id
+            cb |> Option.iter (fun x -> pendingQueue.Remove x |> ignore)
+            cb
+
+        checkWaiting ()
+        |> Option.orElseWith checkPendingQueue
+        |> function
+        | None -> printfn "Warning: message not handled"
+        | Some cb -> cb.Callback.SetResult cb.Command
+
+    let acquireWaitingLock fn p =
+        task {
+            do! waitingLock.WaitAsync()
+            let result = fn p
+            waitingLock.Release() |> ignore
+            return result
+        }
+    let setWaiting cb =
+        acquireWaitingLock
+            ( fun () ->
+                match waiting with
+                | None -> waiting <- Some cb
+                | Some _ -> failwith "Other command is waiting"
+            ) ()
+    let receive (inbox: MailboxProcessor<ISonicCommand * AsyncReplyChannel<TaskCompletionSource<_>>>) =
+        let receiveCmdTask =
+            task {
+                while not disposed do
+                    let! (cmd, ch) = inbox.Receive()
+                    let cb = {
+                        Command = cmd
+                        Callback = TaskCompletionSource()
+                    }
+                    do! setWaiting cb
+                    do! sendMsgFn (cmd.ToCommandString())
+                    ch.Reply cb.Callback
+                return ()
+            }
+        receiveCmdTask
+        |> Async.AwaitTask
+    let cleanup (disposing: bool) =
+        if not disposed then
+            disposed <- true
+            if disposing then
+                waitingLock.Dispose()
+
+    member _.Initialize() =
+        mailbox <- MailboxProcessor.Start receive |> Some
+
+    member _.OnResponseArrived msg =
+        acquireWaitingLock onResponseArrived msg
+
+    member _.ExecuteCommandAsync cmd =
+        let mailbox = ensureInitialized ()
+        task {
+            let! task = mailbox.PostAndAsyncReply(fun ch -> cmd, ch)
+            return! task.Task
+        }
+    interface IDisposable with
+        override this.Dispose() =
+            cleanup true
+            GC.SuppressFinalize this
--- SonicChannel/MessageTransceiver.fs
+++ SonicChannel/MessageTransceiver.fs
@@ -6,14 +6,10 @@ open System.Buffers
 open System.IO
 open System.IO.Pipelines
 open System.Net.Sockets
-open System.Text
 open System.Threading
 open System.Threading.Tasks
 open FSharp.Control.Tasks.V2.ContextInsensitive
 open FSharpx
-open SonicChannel.SonicCommand
-
-type CSList<'a> = System.Collections.Generic.List<'a>
 
 module Util =
     let readLinesAsync (stream: Stream) (transceiverOption: TransceiverOption) (onLineReceived: string -> unit Task) (ct: CancellationToken) =
@@ -60,113 +56,30 @@ module Util =
         writer.WriteLineAsync (buffer, cancellationToken)
 
 
-type SonicCommandCallback =
-    { Command: ISonicCommand; Callback: TaskCompletionSource<ISonicCommand>; }
 type TransceiverContext =
     { Stream: NetworkStream
       Writer: StreamWriter
-      MsgSender: MailboxProcessor<ISonicCommand * AsyncReplyChannel<TaskCompletionSource<ISonicCommand>>> }
+      CommandQueue: CommandQueue
+    }
     with member this.Dispose() =
             this.Writer.Dispose()
+            (this.CommandQueue :> IDisposable).Dispose()
 type MessageTransceiver(client: TcpClient, optionReader: IOptionReader) =
     let opt = optionReader.TransceiverOption()
     let mutable disposed = false
-    let mutable waiting: SonicCommandCallback option = None
     let mutable state : TransceiverContext option = None
     let ensureInitialized () =
         Option.getOrRaise (InvalidOperationException("Transceiver not initialized")) state
-    let waitingLock = new SemaphoreSlim(1, 1)
     let bufferSize = opt.BufferSize
     let encoding = opt.Encoding
-    let pendingQueue = CSList<SonicCommandCallback>()
     let cts = new CancellationTokenSource()
-    let onResponseArrived (line: string) =
-        let handleMessage (cb: SonicCommandCallback) (handleFn: string -> MessageHandleResult) =
-            let handleResult = handleFn line
-            match handleResult with
-            | Bypass -> None
-            | Handled state ->
-                match state with
-                | Pending ->
-                    pendingQueue.Add cb
-                    waiting <- None
-                | Finished ->
-                    waiting <- None
-                Some cb
 
-        let checkWaiting () =
-            waiting
-            |> Option.bind (fun waiting -> handleMessage waiting waiting.Command.HandleWaitingMsg)
-
-        let checkPendingQueue () =
-            let cb =
-                pendingQueue
-                |> Seq.map (fun cb -> handleMessage cb cb.Command.HandlePendingMsg)
-                |> Seq.tryFind (Option.isSome)
-                |> Option.bind id
-            cb |> Option.iter (fun x -> pendingQueue.Remove x |> ignore)
-            cb
-
-        checkWaiting ()
-        |> Option.orElseWith checkPendingQueue
-        |> function
-        | None -> printfn "Warning: message not handled"
-        | Some cb -> cb.Callback.SetResult cb.Command
-
-    let aquireWaitingLock fn p =
-        task {
-            do! waitingLock.WaitAsync()
-            let result = fn p
-            waitingLock.Release() |> ignore
-            return result
-        }
-    let setWaiting cb =
-        aquireWaitingLock
-            ( fun () ->
-                match waiting with
-                | None -> waiting <- Some cb
-                | Some _ -> failwith "Other command is waiting"
-            ) ()
-    let receive (inbox: MailboxProcessor<ISonicCommand * AsyncReplyChannel<TaskCompletionSource<_>>>) =
-        let state = ensureInitialized()
-        let stream = state.Stream
-        let writer = state.Writer
-        let receiveMsgTask =
-            Util.readLinesAsync
-                stream
-                (optionReader.TransceiverOption())
-                (aquireWaitingLock onResponseArrived)
-                cts.Token
-        let receiveCmdTask =
-            task {
-                while not disposed do
-                    let! (cmd, ch) = inbox.Receive()
-                    let cb = {
-                        Command = cmd
-                        Callback = TaskCompletionSource()
-                    }
-                    do! setWaiting cb
-                    do! writer.WriteLineAsync (cmd.ToString())
-                    ch.Reply cb.Callback
-                return ()
-            }
-        Task.WhenAll (receiveCmdTask, receiveMsgTask)
-        |> Task.Ignore
-        |> Async.AwaitTask
-    let sendAsync (cmd: ISonicCommand) =
-        let state = ensureInitialized ()
-        let msgSender = state.MsgSender
-        task {
-            let! task = msgSender.PostAndAsyncReply(fun ch -> cmd, ch)
-            return! task.Task
-        }
     let cleanup (disposing: bool) =
         if not disposed then
             disposed <- true
             if disposing then
                 cts.Cancel()
                 cts.Dispose()
-                waitingLock.Dispose()
                 match state with
                 | Some x -> x.Dispose()
                 | _ -> ()
@@ -178,11 +91,24 @@ type MessageTransceiver(client: TcpClient, optionReader: IOptionReader) =
             let stream = client.GetStream()
             let writer = new StreamWriter(stream, encoding, bufferSize, true)
             writer.AutoFlush <- true
-            let msgSender = MailboxProcessor.Start receive
-            state <- Some { Stream = stream; Writer = writer; MsgSender = msgSender }
+            let sendMsgFn (msg: string) =
+                writer.WriteLineAsync msg
+                |> Task.ToTaskUnit
+            let commandQueue = new CommandQueue(sendMsgFn)
+            commandQueue.Initialize()
+            let receiveMsgTask () =
+                Util.readLinesAsync
+                    stream
+                    (optionReader.TransceiverOption())
+                    commandQueue.OnResponseArrived
+                    cts.Token
+            Task.Run<unit> (Func<Task<unit>> receiveMsgTask)
+            |> ignore
+            state <- Some { Stream = stream; Writer = writer; CommandQueue = commandQueue }
         }
     member _.SendAsync(cmd) =
-        sendAsync cmd
+        let state = ensureInitialized ()
+        state.CommandQueue.ExecuteCommandAsync cmd
     interface IDisposable with
         member this.Dispose() =
             cleanup true
