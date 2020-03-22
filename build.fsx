17c1571c02c8242c41e8e3be721c7c424f5a9100
ZeekoZhu <vaezt@outlook.com>
2020-03-22 18:48:03 +0800

    build: add fake build scripts

--- build.fsx
+++ build.fsx
@@ -6,10 +6,46 @@ open Fake.DotNet
 open Fake.IO
 open Fake.MyFakeTools
 open Fake.Core.TargetOperators
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
 
+module Project =
+    let internal resolveProjectFile name =
+        name </> (sprintf "%s.fsproj" name)
+    let SonicChannel = "SonicChannel"
+    let projects =
+        [
+            SonicChannel
+            "SonicConsole"
+        ]
+        |> Seq.map resolveProjectFile
+    
+    let packages =
+        [
+            SonicChannel
+        ]
+
+    let build () =
+        let buildProject project =
+            DotNet.build id project
+        projects
+        |> Seq.iter buildProject
+
+    let pack () =
+        let setOpt (x: Paket.PaketPackParams) =
+            { x with
+                OutputPath = "./output"
+                LockDependencies = true
+            }
+        Paket.pack setOpt
+        
+Target.create "build" (ignore >> Project.build)
+Target.create "pack" (ignore >> Project.pack)
+"build" ==> "pack"
 Target.useTriggerCI ()
 
 Target.create "empty" ignore
+
 
 // start build
 Target.runOrDefaultWithArguments "empty"
